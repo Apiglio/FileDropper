ed6c70e6ff3e059e0cefbd6868fcaf127697cd20
Apemiro <1157775623@qq.com>
2025-06-06 14:53:08 +0800

    新增Formatter代码; 调整UI; Opener支持一次性打开多个文件; 使用IDropTarget接口

--- file_dropper_main.pas
+++ file_dropper_main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
     Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs,
-    StdCtrls, ExtCtrls, file_dropper;
+    StdCtrls, ExtCtrls, file_dropper, comobj, windows, ActiveX, LazUTF8;
 
 type
 
@@ -21,9 +21,12 @@ type
         procedure DoResize(Sender: TObject);
         procedure DoMouseEnter(Sender: TObject);
         procedure DoMouseLeave(Sender: TObject);
+        procedure DoDragDrop(Sender, Source: TObject; X, Y: Integer);
+        procedure DoDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
         procedure DoPaint(Sender: TObject);
     public
         ButtonId:Integer;
+        Actioner:TObject; // Openner, Archiver or Formatter
     public
         procedure LoadIcon(icon:Graphics.TBitmap);
         procedure SetTitle(Title:string);
@@ -35,35 +38,50 @@ type
 
     { TForm_FileDropper }
 
-    TForm_FileDropper = class(TForm)
-      Memo_FileInfo: TMemo;
-      Memo_Formatter_Tmp: TMemo;
-      Panel_Readonly: TPanel;
-      Panel_FormatterDetailed: TPanel;
-      Panel_ArchiverDetailed: TPanel;
-      Panel_Formatter: TPanel;
-      Panel_Archiver: TPanel;
-        Panel_OpennerDetailed: TPanel;
+    TForm_FileDropper = class(TForm, IDropTarget)
+        Memo_FileInfo: TMemo;
+        Memo_Formatter_Example: TMemo;
+        Panel_Readonly: TPanel;
+        Panel_FormatterDetailed: TPanel;
+        Panel_ArchiverDetailed: TPanel;
+        Panel_Formatter: TPanel;
+        Panel_Archiver: TPanel;
+        Panel_OpenerDetailed: TPanel;
         Panel_Opener: TPanel;
         Panel_Hidden: TPanel;
         procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
         procedure FormCreate(Sender: TObject);
         procedure FormDeactivate(Sender: TObject);
+        procedure FormDragDrop(Sender, Source: TObject; X, Y: Integer);
+        procedure FormDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
         procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
         procedure Panel_ArchiverClick(Sender: TObject);
+        procedure Panel_ArchiverDragDrop(Sender, Source: TObject; X, Y: Integer);
+        procedure Panel_ArchiverDragOver(Sender, Source: TObject; X,Y: Integer; State: TDragState; var Accept: Boolean);
         procedure Panel_ArchiverMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
         procedure Panel_FormatterClick(Sender: TObject);
+        procedure Panel_FormatterDetailedResize(Sender: TObject);
+        procedure Panel_FormatterDragDrop(Sender, Source: TObject; X, Y: Integer);
+        procedure Panel_FormatterDragOver(Sender, Source: TObject; X,Y: Integer; State: TDragState; var Accept: Boolean);
         procedure Panel_FormatterMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
-        procedure Panel_OpennerDetailedResize(Sender: TObject);
+        procedure Panel_OpenerDetailedResize(Sender: TObject);
         procedure Panel_OpenerClick(Sender: TObject);
+        procedure Panel_OpenerDragDrop(Sender, Source: TObject; X, Y: Integer);
+        procedure Panel_OpenerDragOver(Sender, Source: TObject; X, Y: Integer;State: TDragState; var Accept: Boolean);
         procedure Panel_OpenerMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
     private
         FDetailedButtons:TList;
         procedure DetailedButtonClick(Sender: TObject);
         procedure DetailedMouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
 
+    private
+        function DragEnter(const dataObj: IDataObject; grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult;StdCall;
+        function DragOver(grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult;StdCall;
+        function DragLeave: HResult;StdCall;
+        function Drop(const dataObj: IDataObject; grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD):HResult;StdCall;
     protected
         FPhase: TFileDropperPhase;
+        FCurrentFiles: array of string;
     public
         procedure Phase_Overview;
         procedure Phase_Openner;
@@ -71,14 +89,15 @@ type
         procedure Phase_Formatter;
     end;
 
+    function ILCombine(pidl1, pidl2: PItemIDList): PItemIDList; stdcall; external 'shell32.dll' name 'ILCombine';
+
 var
     Form_FileDropper: TForm_FileDropper;
     FileDropper:TFileDropper;
-    CurrentFileName:string;
+    //CurrentFileName:string;
     ReleaseAction:boolean;
 
 implementation
-uses Windows;
 
 {$R *.lfm}
 
@@ -108,6 +127,16 @@ procedure TDetailedButton.DoMouseEnter(Sender: TObject);
 begin
     FHover:=true;
     Paint;
+end;
+
+procedure TDetailedButton.DoDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+    OnDragDrop(Sender, Source, X, Y)
+end;
+
+procedure TDetailedButton.DoDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
+begin
+    DoDragOver(Sender, Source, X, Y, State, Accept);
 end;
 
 procedure TDetailedButton.DoMouseLeave(Sender: TObject);
@@ -191,43 +220,73 @@ begin
 
     FileDropper.SaveProperties;
     FileDropper.Free;
+
+    //{
+    RevokeDragDrop(Self.Handle);
+    OleUninitialize;
+    //}
 end;
 
 procedure TForm_FileDropper.FormCreate(Sender: TObject);
-var idx,h4,w4:integer;
+var idx:integer;
     newDetailedButton:TDetailedButton;
     tmpOpener:TFileDropperOpener;
+    tmpFormatter:TFileDropperFormatter;
 begin
+    //{
+    OleInitialize(nil);
+    RegisterDragDrop(Self.Handle, Self as IDropTarget);
+    DragAcceptFiles(Self.Handle, true);
+    //}
+    //AllowDropFiles:=true;
+
     FileDropper:=TFileDropper.Create;
     FileDropper.LoadProperties;
-    //强行补到16个
+    FDetailedButtons:=TList.Create;
+
+    // 强行补到16个
+    // Opener
     while FileDropper.GetOpenerCount<16 do begin
         tmpOpener:=TFileDropperOpener.Create;
         tmpOpener.DisplayName:='';
-        //tmpOpener.DisplayIcon:='';
         tmpOpener.ExecuteFile:='';
         FileDropper.AddOpenner(tmpOpener);
     end;
-
-    h4:=Panel_OpennerDetailed.Height div 4;
-    w4:=Panel_OpennerDetailed.Width div 4;
-    FDetailedButtons:=TList.Create;
     for idx:=0 to 15 do begin
         newDetailedButton:=TDetailedButton.Create(Self);
-        newDetailedButton.Parent:=Panel_OpennerDetailed;
-        newDetailedButton.Top:=(idx div 4) * h4;
-        newDetailedButton.Left:=(idx mod 4) * w4;
-        newDetailedButton.Width:=w4;
-        newDetailedButton.Height:=h4;
+        newDetailedButton.Parent:=Panel_OpenerDetailed;
         newDetailedButton.ButtonId:=idx;
         newDetailedButton.OnClick:=@DetailedButtonClick;
         newDetailedButton.OnMouseUp:=@DetailedMouseUp;
         FDetailedButtons.Add(newDetailedButton);
-        tmpOpener:=FileDropper.GetOpenerById(idx);
+        newDetailedButton.Actioner:=FileDropper.GetOpenerById(idx);
+        tmpOpener:=newDetailedButton.Actioner as TFileDropperOpener;
         if tmpOpener<>nil then begin
             newDetailedButton.SetTitle(tmpOpener.DisplayName);
             tmpOpener.ReloadIcon;
             newDetailedButton.LoadIcon(tmpOpener.DisplayIcon);
+        end;
+    end;
+
+    //Formatter
+    while FileDropper.GetFormatterCount<16 do begin
+        tmpFormatter:=TFileDropperFormatter.Create;
+        tmpFormatter.DisplayName:='';
+        tmpFormatter.OriginText:='';
+        tmpFormatter.ReplaceText:='';
+        FileDropper.AddFormatter(tmpFormatter);
+    end;
+    for idx:=0 to 15 do begin
+        newDetailedButton:=TDetailedButton.Create(Self);
+        newDetailedButton.Parent:=Panel_FormatterDetailed;
+        newDetailedButton.ButtonId:=idx;
+        newDetailedButton.OnClick:=@DetailedButtonClick;
+        newDetailedButton.OnMouseUp:=@DetailedMouseUp;
+        FDetailedButtons.Add(newDetailedButton);
+        newDetailedButton.Actioner:=FileDropper.GetFormatterById(idx);
+        tmpFormatter:=newDetailedButton.Actioner as TFileDropperFormatter;
+        if tmpFormatter<>nil then begin
+            newDetailedButton.SetTitle(tmpFormatter.DisplayName);
         end;
     end;
 
@@ -240,11 +299,21 @@ begin
     Phase_Overview;
 end;
 
+procedure TForm_FileDropper.FormDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+    //ShowMessage('AA');
+end;
+
+procedure TForm_FileDropper.FormDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
+begin
+    Accept:=true;
+end;
+
 procedure TForm_FileDropper.FormDropFiles(Sender: TObject; const FileNames: array of String);
 begin
-    if Length(FileNames)<>1 then exit;
-    CurrentFileName:=FileNames[0];
-    ReleaseAction:=true;
+    //if Length(FileNames)<>1 then exit;
+    //CurrentFileName:=FileNames[0];
+    //ReleaseAction:=true;
 
 end;
 
@@ -255,8 +324,18 @@ begin
         ReleaseAction:=false;
     end else begin
         Phase_Archiver;
-        CurrentFileName:='';
+        SetLength(FCurrentFiles,0);
     end;
+end;
+
+procedure TForm_FileDropper.Panel_ArchiverDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+    //DragDrop(Source, X, Y);
+end;
+
+procedure TForm_FileDropper.Panel_ArchiverDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
+begin
+    //DragOver(Source, X, Y, State, Accept);
 end;
 
 procedure TForm_FileDropper.Panel_ArchiverMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
@@ -271,8 +350,32 @@ begin
         ReleaseAction:=false;
     end else begin
         Phase_Formatter;
-        CurrentFileName:='';
+        SetLength(FCurrentFiles,0)
     end;
+end;
+
+procedure TForm_FileDropper.Panel_FormatterDetailedResize(Sender: TObject);
+var idx,h4,w4:integer;
+begin
+    // Formatter 临时采用16-31的偏移量
+    h4:=(Panel_FormatterDetailed.Height - Memo_Formatter_Example.Height - 2) div 4;
+    w4:=Panel_FormatterDetailed.Width div 4;
+    for idx:=0 to 15 do begin
+        TDetailedButton(FDetailedButtons[idx+16]).Top:=Memo_Formatter_Example.Height + (idx div 4) * h4 + 3;
+        TDetailedButton(FDetailedButtons[idx+16]).Left:=(idx mod 4) * w4;
+        TDetailedButton(FDetailedButtons[idx+16]).Width:=w4;
+        TDetailedButton(FDetailedButtons[idx+16]).Height:=h4;
+    end;
+end;
+
+procedure TForm_FileDropper.Panel_FormatterDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+    //DragDrop(Source, X, Y);
+end;
+
+procedure TForm_FileDropper.Panel_FormatterDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
+begin
+    //DragOver(Source, X, Y, State, Accept);
 end;
 
 procedure TForm_FileDropper.Panel_FormatterMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
@@ -280,11 +383,12 @@ begin
     if ReleaseAction then Phase_Formatter;
 end;
 
-procedure TForm_FileDropper.Panel_OpennerDetailedResize(Sender: TObject);
+procedure TForm_FileDropper.Panel_OpenerDetailedResize(Sender: TObject);
 var idx,h4,w4:integer;
 begin
-    h4:=Panel_OpennerDetailed.Height div 4;
-    w4:=Panel_OpennerDetailed.Width div 4;
+    // Opener
+    h4:=Panel_OpenerDetailed.Height div 4;
+    w4:=Panel_OpenerDetailed.Width div 4;
     for idx:=0 to 15 do begin
         TDetailedButton(FDetailedButtons[idx]).Top:=(idx div 4) * h4;
         TDetailedButton(FDetailedButtons[idx]).Left:=(idx mod 4) * w4;
@@ -300,8 +404,18 @@ begin
         ReleaseAction:=false;
     end else begin
         Phase_Openner;
-        CurrentFileName:='';
+        SetLength(FCurrentFiles,0);
     end;
+end;
+
+procedure TForm_FileDropper.Panel_OpenerDragDrop(Sender, Source: TObject; X, Y: Integer);
+begin
+    //DragDrop(Source, X, Y);
+end;
+
+procedure TForm_FileDropper.Panel_OpenerDragOver(Sender, Source: TObject; X, Y: Integer; State: TDragState; var Accept: Boolean);
+begin
+    //DragOver(Source, X, Y, State, Accept);
 end;
 
 procedure TForm_FileDropper.Panel_OpenerMouseMove(Sender: TObject;
@@ -311,33 +425,136 @@ begin
 end;
 
 procedure TForm_FileDropper.DetailedButtonClick(Sender: TObject);
+var buttonId:Integer;
+    tmpButton:TDetailedButton;
 begin
     if not ReleaseAction then exit;
-    if CurrentFileName<>'' then FileDropper.Open(CurrentFileName,(Sender as TDetailedButton).ButtonId);
+
+    tmpButton:=Sender as TDetailedButton;
+    buttonId:=tmpButton.ButtonId;
+    if tmpButton.Actioner=nil then exit;
+    if Length(FCurrentFiles)>0 then begin
+        if tmpButton.Actioner is TFileDropperOpener then begin
+            FileDropper.OpenWinCPs(FCurrentFiles,(Sender as TDetailedButton).ButtonId);
+        end else if tmpButton.Actioner is TFileDropperFormatter then begin
+            FileDropper.Format(Memo_Formatter_Example.Lines,(Sender as TDetailedButton).ButtonId);
+        end else begin
+            //无效Actioner
+        end;
+    end;
     ReleaseAction:=false;
     Phase_Overview;
 end;
 
 procedure TForm_FileDropper.DetailedMouseUp(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
 var buttonId:Integer;
+    tmpButton:TDetailedButton;
     tmpOpener:TFileDropperOpener;
-    old_execfile, new_execfile, old_dispname, new_dispname, old_params, new_params:string;
+    tmpFormmater:TFileDropperFormatter;
+    old_s1, new_s1, old_s2, new_s2, old_s3, new_s3:string;
 begin
     if (Button=mbRight) and (Shift = []) then begin
-        buttonId:=TDetailedButton(Sender as TDetailedButton).ButtonId;
-        tmpOpener:=FileDropper.GetOpenerById(buttonId);
-        old_execfile:=tmpOpener.ExecuteFile;
-        old_params:=  tmpOpener.RunParameters;
-        old_dispname:=tmpOpener.DisplayName;
-        new_execfile:=InputBox('修改打开方式','执行文件路径：',old_execfile);
-        if (new_execfile<>'') and (new_execfile<>old_execfile) then tmpOpener.ExecuteFile:=new_execfile;
-        new_params:=  InputBox('修改打开方式','程序执行参数：',old_params);
-        if (new_params<>'')   and (new_params<>old_params) then tmpOpener.RunParameters:=new_params;
-        new_dispname:=InputBox('修改打开方式','按键提示文字：',old_dispname);
-        if (new_dispname<>'') and (new_dispname<>old_dispname) then tmpOpener.DisplayName:=new_dispname;
-        (Sender as TDetailedButton).SetTitle(new_dispname);
-        tmpOpener.ReloadIcon;
-        (Sender as TDetailedButton).LoadIcon(tmpOpener.DisplayIcon);
+        tmpButton:=Sender as TDetailedButton;
+        buttonId:=tmpButton.ButtonId;
+        if tmpButton.Actioner=nil then exit;
+
+        if tmpButton.Actioner is TFileDropperOpener then begin
+            tmpOpener:=FileDropper.GetOpenerById(buttonId);
+            old_s1:= tmpOpener.ExecuteFile;
+            old_s3:= tmpOpener.RunParameters;
+            old_s2:= tmpOpener.DisplayName;
+            new_s1:= InputBox('修改打开方式','执行文件路径：',old_s1);
+            if (new_s1<>'') and (new_s1<>old_s1) then tmpOpener.ExecuteFile:=new_s1;
+            new_s3:= InputBox('修改打开方式','程序执行参数：',old_s3);
+            if (new_s3<>'')   and (new_s3<>old_s3) then tmpOpener.RunParameters:=new_s3;
+            new_s2:= InputBox('修改打开方式','按键提示文字：',old_s2);
+            if (new_s2<>'') and (new_s2<>old_s2) then tmpOpener.DisplayName:=new_s2;
+            (Sender as TDetailedButton).SetTitle(new_s2);
+            tmpOpener.ReloadIcon;
+            (Sender as TDetailedButton).LoadIcon(tmpOpener.DisplayIcon);
+        end else if tmpButton.Actioner is TFileDropperFormatter then begin
+            tmpFormmater:=FileDropper.GetFormatterById(buttonId);
+            old_s2:= tmpFormmater.OriginText;
+            old_s3:= tmpFormmater.ReplaceText;
+            old_s1:= tmpFormmater.DisplayName;
+            new_s2:= InputBox('修改替换设置','查找字符串：',old_s2);
+            if (new_s2<>'') and (new_s2<>old_s2) then tmpFormmater.OriginText:=new_s2;
+            new_s3:= InputBox('修改替换设置','替换字符串：',old_s3);
+            if (new_s3<>'') and (new_s3<>old_s3) then tmpFormmater.OriginText:=new_s3;
+            new_s1:= InputBox('修改替换设置','按键提示文字：',old_s1);
+            if (new_s1<>'')   and (new_s1<>old_s1) then tmpOpener.RunParameters:=new_s1;
+            (Sender as TDetailedButton).SetTitle(new_s1);
+
+        end else begin
+            //无效Actioner
+        end;
+    end;
+end;
+
+function TForm_FileDropper.DragEnter(const dataObj: IDataObject; grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult; StdCall;
+begin
+    result:=S_OK;
+end;
+
+function TForm_FileDropper.DragOver(grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult; StdCall;
+begin
+    dwEffect:=DROPEFFECT_COPY;
+    result:=S_OK;
+end;
+
+function TForm_FileDropper.DragLeave: HResult; StdCall;
+begin
+    result:=S_OK;
+end;
+
+function TForm_FileDropper.Drop(const dataObj: IDataObject; grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult; StdCall;
+var EnumFormatEtc: IEnumFormatEtc;
+    FormatEtc: TFormatEtc;
+    Fetched: LongInt;
+    Medium: TStgMedium;
+    //format_name : array[0..255] of char;
+    tmpDrop: HDROP;
+    FilePath:array[0..MAX_PATH] of char;
+    idx, FileCount:integer;
+    unicode_context:utf8String;
+begin
+    Result := S_OK;
+    //Memo_Formatter_Example.Lines.Clear;
+    if not dataObj.EnumFormatEtc(DATADIR_GET, EnumFormatEtc) = S_OK then exit;
+    while EnumFormatEtc.Next(1, FormatEtc, @Fetched) = S_OK do begin
+        //GetClipboardFormatName(FormatEtc.cfFormat, @format_name[0], 255);
+        //Memo_Formatter_Example.Lines.Add(IntToStr(FormatEtc.cfFormat)+' '+pchar(@format_name[0]));
+        case FormatEtc.cfFormat of
+            CF_HDROP: // InShellDragLoop
+            begin
+                SetLength(FCurrentFiles,0);
+                if dataObj.GetData(FormatEtc, Medium) = S_OK then
+                try
+                    tmpDrop := HDROP(Medium.hGlobal);
+                    FileCount := DragQueryFile(tmpDrop, $FFFFFFFF, nil, 0);
+                    SetLength(FCurrentFiles, FileCount);
+                    for idx := 0 to FileCount - 1 do begin
+                        DragQueryFile(tmpDrop, idx, @FilePath[0], MAX_PATH);
+                        FCurrentFiles[idx] := FilePath;
+                    end;
+                    ReleaseAction:=true;
+                finally
+                    ReleaseStgMedium(Medium);
+                end;
+            end;
+            CF_UNICODETEXT:
+            begin
+                if dataObj.GetData(FormatEtc, Medium) = S_OK then
+                try
+                    unicode_context:=PUnicodeChar(GlobalLock(Medium.hGlobal));
+                    Memo_Formatter_Example.Lines.Clear;
+                    Memo_Formatter_Example.Lines.add(unicode_context);
+                    GlobalUnlock(Medium.hGlobal);
+                finally
+                    ReleaseStgMedium(Medium);
+                end;
+            end;
+        end;
     end;
 end;
 
